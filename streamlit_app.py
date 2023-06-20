b49954d06083159d83cbf0e33eaae8c7096e7aef
Abilash Augustus <86665269+Abilash-augustus@users.noreply.github.com>
2023-06-21 00:20:31 +0530

    Update streamlit_app.py
    add pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit
+import pandas
 
-streamlit.title('My Mom"'"s New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothe')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado Toast')
+streamlit.header('Build your own fruit smoothe')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
 
 
